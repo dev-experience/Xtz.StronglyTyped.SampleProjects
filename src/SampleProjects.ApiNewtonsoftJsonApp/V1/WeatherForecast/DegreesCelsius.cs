bbc6734c98957413946dc086bb60ad9e11181ea4
Vlad DX <vlad.dev.experience@gmail.com>
2021-07-17 21:40:33 +0200

    Add new test project for .NET Core 3.1 API and upgrade NuGet packages

---
+++ src/SampleProjects.ApiNewtonsoftJsonApp/V1/WeatherForecast/DegreesCelsius.cs
@@ -0,0 +1,11 @@
+using Xtz.StronglyTyped.SourceGenerator;
+
+namespace SampleProjects.ApiNewtonsoftJsonApp.V1.WeatherForecast
+{
+    // NOTE: Strong type based on `int`
+    // NOTE: `StrongTypeAttribute` triggers source generation on compilation. All strongly-typed functionality is generated in a partial class
+    [StrongType(typeof(int))]
+    public partial class DegreesCelsius
+    {
+    }
+}
